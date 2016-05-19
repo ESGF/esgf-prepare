13773e4286d83dcc9cec393de50c5c12a4c89d1f
Guillaume <glipsl@ipsl.jussieu.fr>
2016-05-19 15:25:03 +0200

    Delete version format enforcement

--- esgprep/utils/parser.py
+++ esgprep/utils/parser.py
@@ -57,7 +57,7 @@ def translate_directory_format(directory_format_raw):
     # Constraint on %(ensemble)s variable
     pattern = re.sub(re.compile(r'%\((ensemble)\)s'), r'(?P<ensemble>r[\d]+i[\d]+p[\d]+)', pattern)
     # Constraint on %(version)s number
-    pattern = re.sub(re.compile(r'%\((version)\)s'), r'(?P<version>v[\d{8}]+|latest)', pattern)
+    pattern = re.sub(re.compile(r'%\((version)\)s'), r'(?P<version>v[\d]+|latest)', pattern)
     # Translate all patterns matching %(name)s
     pattern = re.sub(re.compile(r'%\(([^()]*)\)s'), r'(?P<\1>[\w.-]+)', pattern)
     return '{0}/(?P<filename>[\w.-]+\.nc)'.format(pattern)
