4b8b288543668cddeef87045ef337752cb1d4ad8
Guillaume <glipsl@ipsl.jussieu.fr>
2016-09-22 16:09:21 +0200

    bump version + requirements fix

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='esgprep',
-      version='2.5.4',
+      version='2.5.5',
       description='Toolbox to prepare data for ESGF publication',
       author='Levavasseur Guillaume',
       author_email='glipsl@ipsl.jussieu.fr',
       url='https://github.com/IS-ENES-Data/esgf-prepare',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=['lockfile>=0.10.2', 'requests>=2.9.1', 'github3.py>=0.9.5'],
+      install_requires=['lockfile>=0.10.2', 'requests>=2.9.1', 'github3.py==0.9.5', 'uritemplate.py<=2.0.0'],
       platforms=['Unix'],
       zip_safe=False,
       entry_points={'console_scripts': ['esgprep=esgprep.esgprep:run']},
