2ea3ea6fa0fd30b553bb4c90517d78f0fa99634c
Guillaume <glipsl@ipsl.fr>
2018-05-18 23:21:42 +0200

    Typo

--- esgprep/utils/constants.py
+++ esgprep/utils/constants.py
@@ -643,7 +643,7 @@ CHECKSUMS_FROM_HELP = \
     """
     Get the checksums from an submitted file. This checksum|n
     file must have the same format as the output of the UNIX|n
-    command-lines "*sum". If the case of unfound checksums,|n
+    command-lines "*sum". In the case of unfound checksums,|n
     it falls back to compute the checksum as normal.
 
     """
