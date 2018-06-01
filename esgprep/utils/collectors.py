5b6fc3d88598bb2c6ada7045eda29da6c4003358
Alan Iwi <alan.iwi@stfc.ac.uk>
2018-06-01 18:57:53 +0100

    do not write control characters when spinner=False (messes up esgmapfile show --quiet)

--- esgprep/utils/collectors.py
+++ esgprep/utils/collectors.py
@@ -79,8 +79,9 @@ class Collector(object):
         for _ in self.__iter__():
             progress.next()
             s += 1
-        sys.stdout.write('\r\033[K')
-        sys.stdout.flush()
+        if self.spinner:
+            sys.stdout.write('\r\033[K')
+            sys.stdout.flush()
         return s
 
     def attach(self, item):
