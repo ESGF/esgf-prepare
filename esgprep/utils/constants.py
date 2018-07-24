2f8093dfa7cc14ba1329daecf55a4f277c7fc869
Guillaume <glipsl@ipsl.fr>
2018-07-24 10:33:37 +0200

    Update date

--- esgprep/utils/constants.py
+++ esgprep/utils/constants.py
@@ -13,7 +13,7 @@ from datetime import datetime
 VERSION = '2.9.0'
 
 # Date
-VERSION_DATE = datetime(year=2018, month=7, day=23).strftime("%Y-%d-%m")
+VERSION_DATE = datetime(year=2018, month=7, day=24).strftime("%Y-%d-%m")
 
 # Shell colors map
 SHELL_COLORS = {'red': 1,
