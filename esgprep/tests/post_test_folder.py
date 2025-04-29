fc769f32f60cb6a4056cc37681482b3057431265
TROUSSELLIER Laurent <laurent.troussellier@ipsl.fr>
2025-04-29 13:58:05 +0200

    chore : cleaning

--- esgprep/tests/post_test_folder.py
+++ esgprep/tests/post_test_folder.py
@@ -1,28 +1,34 @@
-from pathlib import Path
 import re
+from pathlib import Path
 
 
 class Post_Test_Folder:
     def __init__(self, folder: Path):
         self.folder = folder
         self.list_dir_base = list(self.folder.glob("*"))
-        self.list_version = None
-        self.contains_at_least_the_3_folders()
+        match = re.search("\\d{8}", str(folder))
+        assert match is not None
+        self.list_version = [
+            match.group()
+            for folder in [dir.name for dir in self.list_dir_base]
+            if re.search("\\d{8}", folder) is not None
+        ]
 
     def exists(self):
         return self.folder.exists()
 
     def contains_at_least_the_3_folders(self):  # files/d**** ; v***** and latest
-        list_dir_base = list(self.folder.glob("*"))
-        latest = 'latest' in [dir.name for dir in self.list_dir_base]
-        files = 'files' in [dir.name for dir in self.list_dir_base]
-        self.list_version = [re.search('\\d{8}', folder).group() for folder in [dir.name for dir in self.list_dir_base]
-                             if re.search('\\d{8}', folder) is not None]
+        latest = "latest" in [dir.name for dir in self.list_dir_base]
+        files = "files" in [dir.name for dir in self.list_dir_base]
         return len(self.list_version) > 0 or files or latest
 
-    def is_there_same_number_of_files_in_d_and_v(self): # ODO ... ça passerra pas en cas de mise à jour partielle !
+    def is_there_same_number_of_files_in_d_and_v(
+        self,
+    ):  # TODO ... ça passerra pas en cas de mise à jour partielle !
         files_dir = Path.joinpath(self.folder, "files")
-        list_dversion_in_files = list(files_dir.glob("*"))  # each of d******* in files folder
+        list_dversion_in_files = list(
+            files_dir.glob("*")
+        )  # each of d******* in files folder
         return len(self.list_version) == len(list_dversion_in_files)
 
     def is_there_symlink_between_v_and_d(self):
@@ -40,12 +46,12 @@ class Post_Test_Folder:
         last_version = sorted_version_list[-1]
         last_version_path = Path.joinpath(self.folder, "v" + last_version)
         latest_path = Path.joinpath(self.folder, "latest")
-        # print("last_version_found = ",last_version, last_version_path)
-        # print(latest_path)
-        # print("LE TEST /1 " ,last_version_path, )
-        # print("LE TEST /2 ", latest_path.resolve(), latest_path.resolve().parent )
-        # print(latest_path.exists() , last_version_path.exists() , latest_path.is_symlink() , last_version_path == latest_path.resolve().parent)
-        return latest_path.exists() and last_version_path.exists() and latest_path.is_symlink() and last_version_path == latest_path.resolve()
+        return (
+            latest_path.exists()
+            and last_version_path.exists()
+            and latest_path.is_symlink()
+            and last_version_path == latest_path.resolve()
+        )
 
     def test(self):
         print("-----------------------------")
@@ -53,16 +59,25 @@ class Post_Test_Folder:
 
         assert self.exists()
         print("--- Exist                                                    -------")
-        assert self.contains_at_least_the_3_folders(), "Has NOT files,latest, and at least one version folder"
+        assert self.contains_at_least_the_3_folders(), (
+            "Has NOT files,latest, and at least one version folder"
+        )
         print("--- HAS files,latest, and at least one version folder        -------")
 
-        assert self.is_there_same_number_of_files_in_d_and_v() , "Has NOT same number of files/d******** and v********"
+        assert self.is_there_same_number_of_files_in_d_and_v(), (
+            "Has NOT same number of files/d******** and v********"
+        )
         print("--- HAS same number of files/d******** and v********         -------")
 
-        assert self.is_there_symlink_between_v_and_d(), "Has NOT symlink between files/d********/* and v********/*"
+        assert self.is_there_symlink_between_v_and_d(), (
+            "Has NOT symlink between files/d********/* and v********/*"
+        )
         print("--- HAS symlink between files/d********/* and v********/*    -------")
 
-        assert self.is_there_symlink_between_latest_and_latest_version(), "HAS NOT symlink between latest and the latest v******** "
+        assert self.is_there_symlink_between_latest_and_latest_version(), (
+            "HAS NOT symlink between latest and the latest v******** "
+        )
         print("--- HAS symlink between latest and the latest v********      -------")
 
         print("--- EVERYTHING FINE ")
+
